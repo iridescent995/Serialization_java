ba7862929e89d39f03ef728ad61956e8de6e556b
Harshit <ht2995@gmail.com>
2018-10-20 22:26:51 +0530

    versioning

--- JsonToObject/src/VersionUid2.java
+++ JsonToObject/src/VersionUid2.java
@@ -7,31 +7,32 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 
-
-public class VersionUId {
-
-	public static void main(String[] args) {
+public class VersionUid2 {
+	
+public static void main(String[] args) {
 		
 		test obj = new test();
-		obj.id="123";
-		obj.Name="Harry";
-		
-		obj.getbytes(obj);
+//		obj.id="123";
+//		obj.Name="Harry";
+//		
+//		obj.getbytes(obj);
 		System.out.println("========");
 		//System.out.println(data);
 		
-//		test obj1 = obj.getobj();
-//		System.out.println(obj1.Name);
+		test obj1 = obj.getobj();
+		System.out.println(obj1.id);
 	}
 
 
 }
 
 
-class testw implements Serializable{
+class test implements Serializable{
+	
 static final long serialVersionUID = 1L;
 String id;
 String Name;
+String address;
 
 	//Convert byte array to test
 		test getobj() {
@@ -42,30 +43,30 @@ String Name;
 			obj = (test) is.readObject();
 			is.close();
 			filein.close();
-			System.out.println(obj.Name);
+			System.out.println(obj.address);
 			}catch(Exception e) {
 				System.out.println(e);
 			}
 			return obj;
 		}
 
-	//convert object to bye	
-		void getbytes(test obj) {
-			//byte[] data= null;
-			try {
-				
-				FileOutputStream file = new FileOutputStream("Obj.prop");
-				ObjectOutputStream oos =  new ObjectOutputStream(file);
-				oos.writeObject(obj);
-				oos.close();
-				file.close();
-				System.out.println("written in obj.prop file");
-			}catch(Exception e) {
-				System.out.println(e);
-			}
-			
-			
-		}
+//	//convert object to bye	
+//		void getbytes(test obj) {
+//			//byte[] data= null;
+//			try {
+//				
+//				FileOutputStream file = new FileOutputStream("Obj.prop");
+//				ObjectOutputStream oos =  new ObjectOutputStream(file);
+//				oos.writeObject(obj);
+//				oos.close();
+//				file.close();
+//				System.out.println("written in obj.prop file");
+//			}catch(Exception e) {
+//				System.out.println(e);
+//			}
+//			
+//			
+//		}
 
 
 }
